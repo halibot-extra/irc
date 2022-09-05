1f2ecaf65f9b148875819f479c8a9fb5f5b19e09
Eric Richter <richteer@lastprime.net>
2022-09-05 15:32:40 -0500

    disconnect in the client eventloop, it sometimes matters?

--- irc.py
+++ irc.py
@@ -64,7 +64,7 @@ class IrcAgent(HalAgent):
 		asyncio.ensure_future(self.client.message(msg.whom(), msg.body), loop=self.eventloop)
 
 	def shutdown(self):
-		asyncio.run_coroutine_threadsafe(self.client.disconnect(), self.eventloop)
+		asyncio.run_coroutine_threadsafe(self.client.disconnect(), self.client.eventloop)
 		self.thread.join()
 		# Close this eventloop *after* the join, lets the disconnect() have time to finish
 		#  This may not even need to be called
